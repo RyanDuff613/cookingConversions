88260f88356a6a52201f7b3e253b49040b2e4999
Ryan Duff <ryanduff613@gmail.com>
2020-03-25 15:11:59 -0700

    succesfully brings in user input, parses input and stores value in variable called userGrams. prints value of userGrams in div below

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 
+
   $("form#gramsToOunces").submit(function(event){
     event.preventDefault();
 
+    var userGrams = parseInt($('#inputGrams').val());
+    console.log(userGrams);
+    $('#conversion').text(userGrams);
+  
   });
-
 });
