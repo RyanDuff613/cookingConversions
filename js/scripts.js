52e5fe0e0ff3ff6d639d52291532af58a0c8acbf
Ryan Duff <ryanduff613@gmail.com>
2020-03-25 15:23:04 -0700

    build business logic, succesfully convert grams to ounces and display result to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
 
+  function convertToOunces(userGrams) {
+    return userGrams*0.035274;
+  };
 
   $("form#gramsToOunces").submit(function(event){
     event.preventDefault();
-
     var userGrams = parseInt($('#inputGrams').val());
+    var converted = convertToOunces(userGrams);
     console.log(userGrams);
-    $('#conversion').text(userGrams);
-  
+    console.log(converted)
+    $('#conversion').text(userGrams + ' grams is '+converted+' ounces');
   });
 });
